6364860cf141d7590d20a8c5d7d0cd37817492b5
PatRYCBAR <33760183+PatRYCBAR@users.noreply.github.com>
2019-04-30 15:36:35 -0400

    Added Initiative Order + 3 Functions (wipe, add, search)
    Added:
     - the variable for Initiative Order
     - the ability to wipe it
     - the ability to add new people to it
     - the ability to find a character's location within it

--- mybot.js
+++ mybot.js
@@ -5,12 +5,61 @@ const config = require("./config.json");
 client.on("ready", () => {
   console.log("I am ready!");
 });
- 
-// Set the prefix
-const prefix = "!";
+
+// ======================
+// Global Variables
+// ======================
+
+const prefix = "@battlebot"; // Set the prefix
+var battle_progress_state = 0; // Not in use just yet
+
+var initiative_list = []; // full list of people in combat
+// The list is an array of objects containing the following:
+// user - the Discord user running the character
+// character - the IC identity of the character (defaults to the "user")
+// initiative - the character's Initiative value
+
+// ======================
+// Functions
+// ======================
+
+// test function
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+// for wiping the Initiative list at the end of battle
+function wipeInitiative() {
+  initiative_list = [];
+  return 0;
+}
+
+// Add a new character into the Initiative List in the last slot (sets-up the object, assuming error-checking is handled)
+function addNewCharacter(newuser, newcharacter, newinitiative) {
+  // creates a new object from the arguments and puts it into the initiative order at the end
+  initiative_list.push({user:newuser, character:newcharacter, initiative:newinitiative});
+  // user - the Discord user running the character
+  // character - the IC identity of the character (defaults to the "user")
+  // initiative - the character's Initiative value
+  return 0;
+}
+
+// Takes in the name of the character and returns the index in the Initiative List and -1 if it isn't found
+function findCharacter(charactername) {
+  var list_index = -1;
+  var i;
+  for (i = 0; i < initiative_list.length; ++i){
+    if(initiative_list[i] == charactername) list_index = i;
+  }
+  return list_index;
+}
+
+// ======================
+// Event Handlers
+// ======================
+
 client.on("message", (message) => {
   if (message.author.bot) return;
-  // This is where we'll put our code.
   if (message.content.indexOf(config.prefix) !== 0) return;
  
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
