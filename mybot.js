28bf7fa41acfe67f279345e6731c120a64a92f76
PatRYCBAR <33760183+PatRYCBAR@users.noreply.github.com>
2019-05-02 13:41:19 -0400

    Renamed object from "character" to "combatant"
    Changing in-program nomenclature for object from "character" to "combatant".  This was done to cut down on confusion potentially caused by the mixing it up with the basic "char" datatype.

--- mybot.js
+++ mybot.js
@@ -15,9 +15,9 @@ var battle_progress_state = 0; // Not in use just yet
 
 var initiative_list = []; // full list of people in combat
 // The list is an array of objects containing the following:
-// user - the Discord user running the character
-// character - the IC identity of the character (defaults to the "user")
-// initiative - the character's Initiative value
+// user - the Discord user running the combatant
+// combatant - the IC identity of the combatant (defaults to the "user")
+// initiative - the combatant's Initiative value
 
 // ======================
 // Functions
@@ -34,33 +34,33 @@ function wipeInitiative() {
   return 0;
 }
 
-// Add a new character into the Initiative List in the last slot (sets-up the object, assuming error-checking is handled)
-function addNewCharacter(newuser, newcharacter, newinitiative) {
+// Add a new combatant into the Initiative List in the last slot (sets-up the object, assuming error-checking is handled)
+function addNewCombatant(newuser, newcombatant, newinitiative) {
   // creates a new object from the arguments and puts it into the initiative order at the end
-  initiative_list.push({user:newuser, character:newcharacter, initiative:newinitiative});
-  // user - the Discord user running the character
-  // character - the IC identity of the character (defaults to the "user")
-  // initiative - the character's Initiative value
+  initiative_list.push({user:newuser, combatant:newcombatant, initiative:newinitiative});
+  // user - the Discord user running the combatant
+  // combatant - the IC identity of the combatant (defaults to the "user")
+  // initiative - the combatant's Initiative value
   
-  // returns the newly added character as an object
+  // returns the newly added combatant as an object
   return initiative_list[initiative_list.length - 1];
 }
 
-// Takes in the name of the character and returns the index in the Initiative List and -1 if it isn't found
-function findCharacter(charactername) {
+// Takes in the name of the combatant and returns the index in the Initiative List and -1 if it isn't found
+function findCombatant(combatantname) {
   var list_index = -1;
   var i;
   for (i = 0; i < initiative_list.length; ++i){
-    if(initiative_list[i] == charactername) list_index = i;
+    if(initiative_list[i] == combatantname) list_index = i;
   }
   return list_index;
 }
 
-// WORKING ON: Take in an array of character objects and re-orders it based on the initiative values
+// WORKING ON: Take in an array of combatant objects and re-orders it based on the initiative values
 // https://www.w3schools.com/js/js_array_sort.asp (The Compare Function)
 // function(a, b){return b.initiative - a.initiative} <== goal is from highest to lowest
 
-// WORKING ON: Take in an index number.  Only reorders full "initiative_list" after that index number based on initiative values of characters.
+// WORKING ON: Take in an index number.  Only reorders full "initiative_list" after that index number based on initiative values of combatants.
 // https://www.w3schools.com/js/js_array_methods.asp
 //  - initiative_list.slice(start_index) <== pass to reorder method
 //  - initiative_list = initiative_list.slice(0, start_index).concat(resulting_array) <== assign back to original
